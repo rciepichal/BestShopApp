c299901f0ea2fb88236a6bafa582dc8ec20f49dd
Radek <r.ciepichal@gmail.com>
2022-08-09 19:37:29 +0200

    error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,28 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <Box>
-      <Typography>Error 404</Typography>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexFlow: 'column',
+        p: 5,
+      }}
+    >
+      <Typography component="h4" variant="h4" sx={{ color: 'primary.main' }}>
+        Error 404
+      </Typography>
+      <SentimentVeryDissatisfiedIcon sx={{ fontSize: '2rem' }} />
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <Button variant="outlined" sx={{ mt: 5 }}>
+          <ArrowBackIcon /> Home
+        </Button>
+      </Link>
     </Box>
   );
 };
